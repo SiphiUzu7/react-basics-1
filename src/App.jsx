22bd4aac98446d3bc6e80537987857dfd28f7bfd
Siphiwe Ngidi <mohlabaisiphiwe7@gmail.com>
2026-01-20 15:43:19 +0200

    App layout and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles.css"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Musician Bucket List</h1>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">Musician Name</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add Musician</button>
+      </form>
+      <h2 className="header">My List</h2>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Jimi Hendrix
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            MF DOOM
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
     </>
-  )
-}
 
-export default App
+  )
+  
+}
